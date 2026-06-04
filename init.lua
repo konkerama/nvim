02f569f31b7512e34115053ce34b3adcd859dd2b
konkerama <konkeramaris@gmail.com>
2026-06-04 12:12:36 +0300

    upd

--- init.lua
+++ init.lua
@@ -1300,27 +1300,27 @@ require("lazy").setup({
 	-- 	"neoclide/coc.nvim",
 	-- 	branch = "release",
 	-- },
-	{
-		"NeogitOrg/neogit",
-		lazy = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-
-			-- Only one of these is needed.
-			"sindrets/diffview.nvim", -- optional
-			"esmuellert/codediff.nvim", -- optional
-
-			-- Only one of these is needed.
-			"nvim-telescope/telescope.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-			"nvim-mini/mini.pick", -- optional
-			"folke/snacks.nvim", -- optional
-		},
-		cmd = "Neogit",
-		-- keys = {
-		-- 	{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-		-- },
-	},
+	-- {
+	-- 	"NeogitOrg/neogit",
+	-- 	lazy = true,
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim", -- required
+	--
+	-- 		-- Only one of these is needed.
+	-- 		"sindrets/diffview.nvim", -- optional
+	-- 		"esmuellert/codediff.nvim", -- optional
+	--
+	-- 		-- Only one of these is needed.
+	-- 		"nvim-telescope/telescope.nvim", -- optional
+	-- 		"ibhagwan/fzf-lua", -- optional
+	-- 		"nvim-mini/mini.pick", -- optional
+	-- 		"folke/snacks.nvim", -- optional
+	-- 	},
+	-- 	cmd = "Neogit",
+	-- 	-- keys = {
+	-- 	-- 	{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	-- 	-- },
+	-- },
 	{ "sindrets/diffview.nvim" },
 	-- {
 	-- 	"nvim-lualine/lualine.nvim",
